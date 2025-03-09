5a96d4606fb0d918a267b078d5bf6ffdacd9c031
Coolhatena <ingsc.geffroy@gmail.com>
2025-03-08 18:10:20 -0800

    Create models

---
+++ models/Article.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require('mongoose');
+
+const articleSchema = Schema({
+	title: {
+		type: String,
+		required: true,
+	},
+	content: {
+		type: String,
+		required: true,
+	},
+	date: {
+		type: Date,
+		default: Date.now,
+	},
+	image: {
+		type: String,
+		default: "default.png",
+	},
+});
+
+module.exports = model('Article', articleSchema, 'articles');
