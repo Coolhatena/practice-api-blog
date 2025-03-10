c6ada918af521fee2913afeb13c0bb86e8f67c7c
Coolhatena <ingsc.geffroy@gmail.com>
2025-03-09 22:55:21 -0700

    Add route to get a raw image

--- routes/article.js
+++ routes/article.js
@@ -1,14 +1,30 @@
 const express = require('express');
-const router = express.Router();
-
+const multer = require('multer');
 const articleController = require('../controllers/article');
+
+const router = express.Router();
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, './images/articles');
+	},
+	filename: (req, file, cb) => {
+		cb(null, "article" + Date.now() + file.originalname);
+	}
+})
+
+const uploads = multer({ storage });
+
 
 // Routes
 router.post('/create', articleController.create);
-router.get('/get', articleController.get);
+router.get('/get/:latest?', articleController.get);
+router.get('/article/:id', articleController.one);
+router.delete('/article/:id', articleController.remove);
+router.put('/article/:id', articleController.edit);
+router.post('/upload-image/:id', [uploads.single("file0")], articleController.upload);
+router.get('/image/:file', articleController.image);
 
 // Test route
-router.get('/test', articleController.test);
 router.get('/anthos', articleController.anthos);
 
 module.exports = router;
